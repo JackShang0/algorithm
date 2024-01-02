d9acabbdb63c824dc556d5d0f3c0ddf397c0cfae
shangqj <shangqj@si-tech.com.cn>
2024-01-02 21:21:28 +0800

    easy：输入一个数组和目标target，输出和为target的数的索引

---
+++ src/main/java/com/algorithm/leetcode/hot100/easy/numberSum.java
@@ -0,0 +1,143 @@
+package com.algorithm.leetcode.hot100.easy;
+
+import java.util.HashMap;
+
+/**
+ * @description: 画解算法：1. 两数之和
+ * @author: shangqj
+ * @date: 2024/1/2
+ * @version: 1.0
+ */
+public class numberSum {
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+     * <p>
+     * 你可以按任意顺序返回答案。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[0,1]
+     * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+     * 示例 2：
+     * <p>
+     * 输入：nums = [3,2,4], target = 6
+     * 输出：[1,2]
+     * 示例 3：
+     * <p>
+     * 输入：nums = [3,3], target = 6
+     * 输出：[0,1]
+     * <p>
+     * <p>
+     * 提示：
+     * <p>
+     * 2 <= nums.length <= 104
+     * -109 <= nums[i] <= 109
+     * -109 <= target <= 109
+     * 只会存在一个有效答案
+     * <p>
+     * <p>
+     * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+     */
+    public static void main(String[] args) {
+        //测试用例：*3
+        //int [] nums = {2,7,11,15};
+        //int [] nums = {3,3};
+        int[] nums = {3, 2, 4};
+        //int target = 9;
+        int target = 6;
+        int[] ints1 = twoSum1(nums, target);
+        int[] ints2 = twoSum2(nums, target);
+        /*int[] ints3 = twoSum3(nums, target);
+        int[] ints4 = twoSum4(nums, target);*/
+
+        for (int anInt : ints2) {
+            System.out.println("anInt = " + anInt);
+        }
+    }
+
+    /**
+     * 由于哈希查找的时间复杂度为 O(1)，所以可以利用哈希容器 map 降低时间复杂度
+     * 遍历数组 nums，i 为当前下标，每个值都判断map中是否存在 target-nums[i] 的 key 值
+     * 如果存在则找到了两个值，如果不存在则将当前的 (nums[i],i) 存入 map 中，继续遍历直到找到为止
+     * @param nums 数组
+     * @param target 目标值
+     * @return 如果最终都没有结果则抛出异常
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("no two sum solution");
+    }
+
+    /**
+     * 输入一个数组 和 目标target 输出和为target的数的索引
+     * @param nums 数组
+     * @param target 目标
+     * @return int
+     */
+    public static int[] twoSum4(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])){
+                return new int[]{ map.get(target - nums[i]),i};
+            }
+            map.put(nums[i],i );
+        }
+        throw new IllegalArgumentException("");
+    }
+
+
+
+
+
+
+
+    public static int[] twoSum3(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //对数组遍历
+        for (int i = 0; i < nums.length; i++) {
+            //判断map中是否有 tar - 当前遍历的值
+            if (map.containsKey(target - nums[i])){
+                //返回map中当前差值的索引  以及当前值的索引
+                return new int[]{map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        throw  new IllegalArgumentException("no two sum solution");
+    }
+
+
+    /**
+     * 暴露遍历  时间复杂度为O(n²)
+     * @param nums 数组
+     * @param target 目标值
+     * @return 返回索引
+     */
+    public static int[] twoSum1(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (target == nums[i] + nums[j] && i != j) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/dataStructure/iteration.java
@@ -0,0 +1,35 @@
+package com.dataStructure;
+
+/**
+ * @description: 斐波那契数列  递归   0, 1, 1, 2, 3, 5, 8, 13
+ * @author: shangqj
+ * @date: 2023/12/26
+ * @version: 1.0
+ */
+public class iteration {
+    public static void main(String[] args) {
+        int fib = fib(8);
+        System.out.println("fib = " + fib);
+    }
+
+    /**
+     * 设斐波那契数列的第 𝑛 个数字为 𝑓(𝑛) ，易得两个结论。
+     * ‧ 数列的前两个数字为 𝑓(1) = 0 和 𝑓(2) = 1 。
+     * ‧ 数列中的每个数字是前两个数字的和，即 𝑓(𝑛) = 𝑓(𝑛 − 1) + 𝑓(𝑛 − 2) 。
+     *
+     * @param n 第n个
+     * @return 返回第n个的数
+     */
+    static int fib(int n) {
+
+        //终止条件 f(1)=0  f(2)=1
+        if (n == 1 || n == 2) {
+            return n - 1;
+        }
+
+        //递归调用 f(n)=f(n-1)+f(n-2)
+        int res = fib(n - 1) + fib(n - 2);
+        //返回结果 f(n)
+        return res;
+    }
+}
