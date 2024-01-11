566e2741b51c07f2e99731dddecb4e2213eef0ba
shangqj <shangqj@si-tech.com.cn>
2024-01-11 18:01:38 +0800

    medium #238 除自身以外数组的乘积

---
+++ src/main/java/com/algorithm/leetcode/hot100/_05list/list4.java
@@ -0,0 +1,41 @@
+package com.algorithm.leetcode.hot100._05list;
+
+/**
+ * @description: #238 除自身以外数组的乘积
+ * @author: shangqj
+ * @date: 2024/1/11
+ * @version: 1.0
+ */
+public class list4 {
+
+    /**
+     给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+     题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+     请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+
+     示例 1:
+     输入: nums = [1,2,3,4]
+     输出: [24,12,8,6]
+     示例 2:
+     输入: nums = [-1,1,0,-3,3]
+     输出: [0,0,9,0,0]
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int[] ints = new int[nums.length];
+        for (int i = 0; i < ints.length; i++) {
+//            ints[i] =
+        }
+
+    }
+
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        int[] ints = new int[nums.length];
+
+
+        return ints;
+    }
+}
