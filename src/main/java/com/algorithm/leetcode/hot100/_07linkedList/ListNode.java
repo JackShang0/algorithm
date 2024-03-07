6849211e29543cf44049b991f071910651480cd5
shangqj <shangqj@si-tech.com.cn>
2024-03-07 18:06:33 +0800

    easy #206 反转链表 ac

---
+++ src/main/java/com/algorithm/leetcode/hot100/_07linkedList/ListNode.java
@@ -0,0 +1,11 @@
+package com.algorithm.leetcode.hot100._07linkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ src/main/java/com/algorithm/leetcode/hot100/_07linkedList/linkedList2.java
@@ -0,0 +1,109 @@
+package com.algorithm.leetcode.hot100._07linkedList;
+
+/**
+ * @description: #206 反转链表
+ * @author: shangqj
+ * @date: 2024/3/7
+ * @version: 1.0
+ */
+public class linkedList2 {
+
+    /**
+     * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+     * <p>
+     * 示例 1：
+     * 输入：head = [1,2,3,4,5]
+     * 输出：[5,4,3,2,1]
+     * 示例 2：
+     * 输入：head = [1,2]
+     * 输出：[2,1]
+     * 示例 3：
+     * 输入：head = []
+     * 输出：[]
+     * <p>
+     * <p>
+     * 提示：
+     * 链表中节点的数目范围是 [0, 5000]
+     * -5000 <= Node.val <= 5000
+     * <p>
+     * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        ListNode listNodeA = new ListNode(4);
+        listNodeA.next = new ListNode(1);
+        listNodeA = listNodeA.next;
+        listNodeA.next = new ListNode(8);
+        listNodeA = listNodeA.next;
+        listNodeA.next = new ListNode(4);
+        listNodeA = listNodeA.next;
+        listNodeA.next = new ListNode(5);
+        if (listNodeA.next != null) {
+
+            System.out.println("listNodeA = " + listNodeA.next);
+        }
+
+        ListNode listNodeB = new ListNode(5);
+        listNodeB.next = new ListNode(6);
+        listNodeB = listNodeB.next;
+        listNodeB.next = new ListNode(1);
+        listNodeB = listNodeB.next;
+        listNodeB.next = new ListNode(8);
+        listNodeB = listNodeB.next;
+        listNodeB.next = new ListNode(4);
+        listNodeB = listNodeB.next;
+        listNodeB.next = new ListNode(5);
+        System.out.println("listNodeA = " + listNodeA);
+        System.out.println("listNodeB = " + listNodeB);
+
+        reverseList1(listNodeA);
+    }
+
+    public static ListNode reverseList1(ListNode listNode) {
+        ListNode preNode = null;
+        ListNode curNode = listNode;
+        while (curNode != null) {
+            ListNode next = curNode.next;
+            //当前节点的下一个节点 指向 preNode
+            curNode.next = preNode;
+            //将 curNode 和 preNode 同时往后移动一个节点
+            preNode = curNode;
+            curNode = next;
+        }
+        return preNode;
+    }
+
+
+    public static ListNode reverseList2(ListNode listNode) {
+
+        ListNode preNode = null, curNode;
+        curNode = listNode;
+        while (curNode != null) {
+            ListNode next = curNode.next;
+            curNode.next = preNode;
+
+            preNode = curNode;
+            curNode = next;
+
+        }
+
+        return preNode;
+    }
+
+
+    public static ListNode reverseList3(ListNode head) {
+
+        ListNode preNode = null,curNode = head;
+        while (curNode != null){
+            ListNode next = curNode.next;
+            curNode.next = preNode;
+
+            preNode = curNode;
+            curNode = next;
+        }
+        return preNode;
+    }
+}
+
