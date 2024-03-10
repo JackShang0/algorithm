f62725a03acd9af3eecc05a911a180f465b792f3
shangdj <15091520295@163.com>
2024-03-10 23:55:42 +0800

    easy #21 合并两个有序链表

---
+++ src/main/java/com/algorithm/leetcode/hot100/_07linkedList/linkedList6.java
@@ -0,0 +1,15 @@
+package com.algorithm.leetcode.hot100._07linkedList;
+
+public class linkedList6 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        return null;
+    }
+
+
+}
