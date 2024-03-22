40d2cfd2ba69c80eb643084d8276e6b31b3c6a28
shangqj <shangqj@si-tech.com.cn>
2024-03-22 18:04:40 +0800

    medium #144 二叉树的前序遍历 ac

---
+++ src/main/java/com/algorithm/leetcode/hot100/_08tree/TreeNode.java
@@ -0,0 +1,14 @@
+package com.algorithm.leetcode.hot100._08tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
